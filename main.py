e7942cdade98224a73c115ff9b100950cfe58099
GaganSingh11 <gsmultani11@gmail.com>
2022-05-04 11:51:52 +0200

    3rd endpoint - post a post

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Optional
 from fastapi import Body, FastAPI
 from pydantic import BaseModel
+from random import randint, randrange
 
 app = FastAPI()
 
@@ -10,6 +11,17 @@ class Post(BaseModel):
     published: bool = True
     rating: Optional[int] = None
 
+post_db = [
+    {
+        "title": "sunny day in yverdon",
+        "body":"I am by hrose satble in yverdon"
+    },
+    {
+        "title":"Indian food provider in Yverdon",
+        "body":"Now all you can eat indian for Fr19.99" 
+    }
+]
+
 
 @app.get("/")
 def root():
@@ -17,9 +29,16 @@ def root():
 
 @app.get("/posts")
 def get_posts():
-    return {"data" : "This is your post"}
+    return {"data" : post_db}
 
-@app.post("/createpost")
+@app.post("/posts")
 def create_post(post: Post):
-    print(post)
-    return {"data": post}
+    post_dict = post.dict()
+    post_dict['id'] = randrange(0, 1000000)
+    post_db.append(post_dict)
+    return {"data": post_dict}
+
+@app.get("/posts/{id}")
+def get_post(id):
+    print(id)
+    return {"post_details" : f"Here is the post for {id}"}
