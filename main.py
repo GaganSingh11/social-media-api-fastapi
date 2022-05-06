61437b7b7ab7f4b26861b8e2f487b836fa604800
GaganSingh11 <gsmultani11@gmail.com>
2022-05-06 11:10:07 +0200

    completed post update end point

--- main.py
+++ main.py
@@ -30,6 +30,12 @@ def find_post(id):
         if p["id"] == id:
             return p
 
+def find_index_post(id):
+    for i,p in enumerate(post_db):
+        if p['id'] == id:
+            return i
+
+
 
 @app.get("/")
 def root():
@@ -59,3 +65,24 @@ def get_post(id: int):
         detail=f"post with id: {id} not found")
     return {"post_details" : post}
 
+@app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
+def delete_post(id: int):
+    #delete post
+    # find the index in array that has required id
+    index = find_index_post(id)
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with {id} does not exist")
+    post_db.pop(index)
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
+
+@app.put("/posts/{id}")
+def update_post(id:int, post:Post):
+    index = find_index_post(id)
+
+    if index == None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail= f"post with {id} does not exist")
+
+    post_dict = post.dict()
+    post_dict['id'] = id
+    post_db[index] = post_dict
+    return {"data":post_dict}
