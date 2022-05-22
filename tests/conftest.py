a9c07d5bc5878d8b53121783e6508d4f97301eea
GaganSingh11 <gsmultani11@gmail.com>
2022-05-22 18:06:21 +0200

    added test cases for create and delete posts endpoints

--- tests/conftest.py
+++ tests/conftest.py
@@ -46,6 +46,17 @@ def client(session):
     yield TestClient(app)
 
 @pytest.fixture
+def test_user2(client):
+    user_data = {"email": "raavi123@gmail.com",
+                "password":"123"}
+    res = client.post("/users/", json=user_data)
+
+    assert res.status_code == 201
+    new_user = res.json()
+    new_user['password'] = user_data["password"]
+    return new_user
+
+@pytest.fixture
 def test_user(client):
     user_data = {"email": "raavi@gmail.com",
                 "password":"123"}
@@ -71,7 +82,7 @@ def authorized_client(client, token):
     return client
 
 @pytest.fixture
-def test_posts(test_user, session):
+def test_posts(test_user, session, test_user2):
     posts_data = [
         {
             "title":'first title',
@@ -87,6 +98,11 @@ def test_posts(test_user, session):
             "title":'third title',
             "body":"third content",
             "owner_id": test_user['id']
+        },
+        {
+            "title":'fourth title',
+            "body":"fourth content",
+            "owner_id": test_user2['id']
         }
     ]
 
--- tests/test_posts.py
+++ tests/test_posts.py
@@ -1,4 +1,5 @@
 from app import schemas
+import pytest
 
 
 def test_get_all_posts(authorized_client, test_posts):
@@ -30,4 +31,68 @@ def test_get_one_post(authorized_client, test_posts):
     assert post.Post.id == test_posts[0].id
     assert post.Post.body == test_posts[0].body
     assert post.Post.title == test_posts[0].title
+
+@pytest.mark.parametrize("title, body, published", [
+    ("awesome new title", "awesome new content", True),
+    ("Italian Pizaa", "I love sausages", True),
+    ("tallest building in Yverdon", "I live there", True),
+])
+def test_create_post(authorized_client, test_user, test_posts, title, body, published):
+    res = authorized_client.post("/posts/", json={"title": title, "body":body, "published": published})
+
+    created_post = schemas.Post(**res.json())
+
+    assert res.status_code == 201
+    assert created_post.title == title  
+    assert created_post.body == body  
+    assert created_post.published == published  
+    assert created_post.owner_id == test_user['id']
+
+def test_create_post_default_published_ture(authorized_client, test_user):
+    res = authorized_client.post("/posts/", json={"title": "random title", "body":"random content"})
+
+    created_post = schemas.Post(**res.json())
+
+    assert res.status_code == 201
+    assert created_post.title == "random title"
+    assert created_post.body == "random content" 
+    assert created_post.published == True 
+    assert created_post.owner_id == test_user['id']
+
     
+def test_unauthorized_user_create_posts(client, test_user, test_posts):
+    
+    res = client.post("/posts/", json={"title": "random title", "body":"random content"})
+
+    assert res.status_code == 401
+
+
+def test_unauthorized_user_delete_posts(client, test_user, test_posts):
+    
+    res = client.delete(f"/posts/{test_posts[0].id}")
+    
+    assert res.status_code == 401
+
+def test_delete_post_success(authorized_client, test_user , test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[0].id}")
+
+    assert res.status_code == 204
+    
+def test_delete_post_non_exist(authorized_client, test_user , test_posts):
+    res = authorized_client.delete(f"/posts/88800000")
+
+    assert res.status_code == 404
+
+def test_delete_other_user_post(authorized_client, test_user , test_posts):
+    res = authorized_client.delete(f"/posts/{test_posts[3].id}")
+
+    assert res.status_code == 403
+
+def test_update_post(authorized_client, test_user , test_posts):
+    data = {
+        "title":"update title",
+        "body":"updated body",
+        "id": test_posts[0].id
+    }
+
+    res = authorized_client.put(f"/posts/{test_posts[0].id}", json=data)
